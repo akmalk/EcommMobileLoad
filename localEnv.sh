a06f354c31e8018e2b3ef5e042636b1ce8abb5e9
rbolton <rbolton@appdynamics.com>
2016-09-19 12:16:59 -0700

    fixing git

--- localEnv.sh
+++ localEnv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export ECOMM_URL=http://ecommhost.com
+export ECOMM_URL=http://localhost
 export EUM_URL=http://col.eum-appdynamics.com
-export EUM_KEY=XX-XXX-XXX-XXX
-export PLATFORM=iOS
-#export APP_NAME=com.appdynamics.ECommerce-iOS1
+export EUM_KEY=AD-AAB-AAB-PZP
+#export PLATFORM=iOS
+export PLATFORM=Android
+#export APP_NAME=com.appdynamics.eum.test.BankingApp
 #export DEBUG_CONSOLE=true
 #export CONSOLE_LOG_LEVEL=debug
 #export LOG_PATH=
